6fb9c1de61cb98d91adc4671f2af360c2c8e3a00
collis olucho <collinsolucho@gmail.com>
2026-03-07 08:51:18 +0300

    added helper functions to helper communication with mongoDb

--- app/model/database.js
+++ app/model/database.js
@@ -1,12 +1,26 @@
 import { client, ObjectId } from "../.server/mongo";
 
-let db = client.db("portfolio");
-let messages = db.collection("messages"); // messages
+let db = client.db("fileDownloader");
+let results = db.collection("schoolResults"); // results
 
-export async function AddMessages(phone, email, message) {
-  return messages.insertOne({
-    phone,
-    message,
-    email,
+//add results
+export async function AddResults(data) {
+  return results.insertOne({
+    ...data,
+    addedAt: new Date(),
   });
+}
+
+// get all results and also return each object id as a string for easy use later
+export async function getResults() {
+  let documents = await results.find().sort({ addedAt: -1 }).toArray();
+  return documents.map((document) => ({
+    ...document,
+    _id: document._id.toString(),
+  }));
+}
+
+//get individual results by using the id given for easy downloading
+export async function getResultById(resultId) {
+  return results.findOne({ _id: ObjectId.createFromHexString(resultId) });
 }
