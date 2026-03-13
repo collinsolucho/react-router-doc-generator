a8c0b12e46733afcb4ce353bb4783504d7023aa3
collis olucho <collinsolucho@gmail.com>
2026-03-13 22:48:36 +0300

    the page can now add data to the db and be used later

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -1,4 +1,12 @@
-import { Form } from "react-router";
+import { Form, Link, useActionData, data, useNavigation } from "react-router";
+import { AddResults } from "../model/database";
+import {
+  commitSession,
+  getSession,
+  setErrorMessage,
+  setSuccessMessage,
+} from "../.server/session";
+import { useEffect, useRef } from "react";
 
 export function meta() {
   return [
@@ -11,14 +19,58 @@ export function meta() {
 }
 
 export async function action({ request }) {
+  const session = await getSession(request.headers.get("Cookie"));
   let formData = await request.formData();
-  console.log(formData);
+  // console.log(formData);
+  let name = formData.get("name");
+  let maths = formData.get("maths");
+  let english = formData.get("english");
+  let swahili = formData.get("swahili");
+  let programming = formData.get("programming");
+  let totals =
+    Number(maths) + Number(english) + Number(swahili) + Number(programming);
+
+  let details = { name, maths, english, swahili, programming, totals };
+  try {
+    let addedResult = await AddResults(details);
+    if (addedResult) {
+      setSuccessMessage(session, "User details have been added");
+
+      // IMPORTANT: You must return a Response with the Header
+      return data(
+        { success: true, student: addedResult },
+        {
+          headers: { "Set-Cookie": await commitSession(session) },
+        }
+      );
+    }
+  } catch (error) {
+    setErrorMessage(session, "Failed to add user Details!");
+    return data(
+      { success: false },
+      {
+        headers: { "Set-Cookie": await commitSession(session) },
+      }
+    );
+  }
 }
 
 export default function Home() {
+  let data = useActionData();
+  //use form.ref to clear the form if data is true from the action data
+  let formRef = useRef();
+  useEffect(() => {
+    if (data?.success) {
+      formRef.current?.reset(); // Clears the form fields
+    }
+  }, [data]);
+  let navigation = useNavigation(); // for submitting state
+  let isSubmitting = navigation.state === "submitting";
   return (
     <main>
       <Form
+        // {formRef}
+        ref={formRef}
         method="post"
         className="max-w-5xl mx-auto min-h-screen justify-center flex flex-col p-5"
       >
@@ -92,9 +144,17 @@ export default function Home() {
           />
         </label>
 
-        <button type="submit" className="border-2 p-1 m-1">
-          Generate pdf
+        <button
+          type="submit"
+          className="border-2 p-1 m-1"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "submitting.." : "submit"}
         </button>
+
+        <Link to="/documents" className="border-2 p-1 m-1 text-center">
+          Download Document ➡️
+        </Link>
       </Form>
     </main>
   );
