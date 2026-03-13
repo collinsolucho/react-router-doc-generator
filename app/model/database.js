7cb63301eebf3a9a538a67af2f1f5cb757593bc1
collis olucho <collinsolucho@gmail.com>
2026-03-13 22:42:06 +0300

    changed getResults to allow pagination

--- app/model/database.js
+++ app/model/database.js
@@ -4,20 +4,43 @@ let db = client.db("fileDownloader");
 let results = db.collection("schoolResults"); // results
 
 //add results
-export async function AddResults(data) {
+export async function AddResults(details) {
   return results.insertOne({
-    ...data,
+    ...details,
     addedAt: new Date(),
   });
 }
 
 // get all results and also return each object id as a string for easy use later
-export async function getResults() {
-  let documents = await results.find().sort({ addedAt: -1 }).toArray();
-  return documents.map((document) => ({
-    ...document,
-    _id: document._id.toString(),
+// have two documents per page for pagination
+export async function getResults(page = 1, pageSize = 2) {
+  // Calculate how many documents to skip
+  // Page 1: (1-1) * 2 = 0 skip
+  // Page 2: (2-1) * 2 = 2 skip
+  const skip = (page - 1) * pageSize;
+
+  const totalDocuments = await results.countDocuments();
+  const totalPages = Math.ceil(totalDocuments / pageSize);
+  //math.ceil(x) method rounds a number up to the nearest integer
+  let documents = await results
+    .find()
+    .sort({ addedAt: -1 })
+    .skip(skip)
+    .limit(pageSize)
+    .toArray();
+
+  const formattedDocs = documents.map((doc) => ({
+    ...doc,
+    _id: doc._id.toString(),
   }));
+
+  return {
+    documents: formattedDocs,
+    totalPages,
+    currentPage: page,
+    hasNextPage: page < totalPages,
+    hasPrevPage: page > 1,
+  };
 }
 
 //get individual results by using the id given for easy downloading
